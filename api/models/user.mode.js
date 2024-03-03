7d4d95d60ffddf44abbd59a4a7001df1edb3de9a
Utkarsh <utkarshmishra101394@gmail.com>
2024-03-04 01:19:13 +0530

    create user model

---
+++ api/models/user.mode.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
