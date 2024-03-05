005683564718513056d46a53e46e6e881bdc9091
Utkarsh <utkarshmishra101394@gmail.com>
2024-03-06 01:07:42 +0530

    complete signup page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,9 +7,9 @@ const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   // hashSync is a synchronous function to hash the password and
   // the value '10' here is the number of round of Salt.
-  // Salt is a variable or number going to be combined with out password and encrypted.
+  // Salt is a variable or number going to be combined with your password and encrypted.
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, hashedPassword });
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
     res.status(201).json("User Created Successfully!");
