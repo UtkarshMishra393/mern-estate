2c0f9e9a85fbe74d57233cc4914e516be83bbdbb
Utkarsh <utkarshmishra101394@gmail.com>
2024-03-19 22:23:39 +0530

    Add Redux Persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+// named the userSlice to userReducer
 import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+// the persisted reducer is created to persist the signed in user's data.
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer,
   },
   // we will add a middleware for making serializable check false
   // so we will not get error for  our variables.
@@ -12,3 +26,5 @@ export const store = configureStore({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
