3b6537fafadcb15501819a7b1c0a0df802cc87b1
Utkarsh <utkarshmishra101394@gmail.com>
2024-03-20 18:20:07 +0530

    Create and Add OAuth Component

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-4d9fc.firebaseapp.com",
+  projectId: "mern-estate-4d9fc",
+  storageBucket: "mern-estate-4d9fc.appspot.com",
+  messagingSenderId: "716963135731",
+  appId: "1:716963135731:web:2fa9505ac7dc801c53b786",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Link }  from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -53,6 +54,7 @@ export default function SignUp() {
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:bg-blue-900 disabled:opacity-50'>
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
+        <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have An Account ?</p>
