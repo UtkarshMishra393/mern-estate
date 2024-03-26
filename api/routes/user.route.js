dcc0eadbecf2b68c161a0efafc7f6020d38fedfe
Utkarsh <utkarshmishra101394@gmail.com>
2024-03-26 20:56:41 +0530

    add delete user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { test, updatedUser } = require("../controllers/user.controller");
+const {
+  test,
+  updatedUser,
+  deleteUser,
+} = require("../controllers/user.controller");
 const verifyToken = require("../utils/verifyUser");
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updatedUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 module.exports = router;
