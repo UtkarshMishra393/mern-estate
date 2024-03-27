bf3ead852dd22895d0ae6a816c341c3ca468304d
Utkarsh <utkarshmishra101394@gmail.com>
2024-03-27 16:28:15 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/s
 import { app } from '../firebase';
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   // useRef is used to reference the image to take the image file input.
@@ -169,6 +170,9 @@ export default function Profile() {
         rounded-lg p-3 uppercase hover:bg-blue-900 disabled:opacity-70'>
           {loading? 'Loading...': 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:bg-green-500' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
 
       <div className='flex justify-between mt-5'>
