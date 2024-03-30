5eee971c82552c3f1704a56888fba54e7d05b0a5
Utkarsh <utkarshmishra101394@gmail.com>
2024-03-31 03:01:45 +0530

    complete upload listing images and create listing page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to="/">
-        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+        <h1 className='hover:shadow-md font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Mishra's</span>
             <span className='text-slate-700'>Estate</span>
         </h1>
@@ -26,7 +26,7 @@ export default function Header() {
           </Link>
           <Link to="/profile">
           { currentUser ? (
-            <img className='rounded-full h-7 w-7 object-cover hover:h-10 hover:w-10' src={currentUser.avatar} alt="profile" />
+            <img className='rounded-full h-7 w-7 object-cover hover:border hover:border-blue-700' src={currentUser.avatar} alt="profile" />
           ): (<li className='text-slate-700 hover:text-blue-700'>Sign In</li>
           )
           }
