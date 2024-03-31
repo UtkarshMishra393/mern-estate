5c4dedcea6342c83e30761b486beba4de72582e3
Utkarsh <utkarshmishra101394@gmail.com>
2024-03-31 20:15:49 +0530

    Complete Update Listing Functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,6 +3,7 @@ const {
   createListng,
   deleteListng,
   updateListing,
+  getListing,
 } = require("../controllers/listing.controller");
 const verifyToken = require("../utils/verifyUser");
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createListng);
 router.delete("/delete/:id", verifyToken, deleteListng);
 router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 
 module.exports = router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -153,7 +153,7 @@ export default function Profile() {
   const handleListingDelete = async (listingId) => {
     try {
       const res = await fetch(`/api/listing/delete/${listingId}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       const data = await res.json();
       if (data.success === false) {
@@ -162,11 +162,12 @@ export default function Profile() {
       }
       // filtering the data that is previusly present in the userListing state.
       setUserListings((prev) =>
-      prev.filter((listing) => listing._id !== listingId));
+        prev.filter((listing) => listing._id !== listingId)
+      );
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -303,8 +304,17 @@ export default function Profile() {
               </Link>
 
               <div className="flex flex-col item-center">
-                <button onClick={() => handleListingDelete(listing._id)} className="text-red-700 uppercase">delete</button>
-                <button className="text-green-700 uppercase">Edit</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase hover:bg-red-700 hover:rounded-md hover:text-white"
+                >
+                  delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase hover:bg-green-700 hover:rounded-lg hover:text-white">
+                    Edit
+                  </button>
+                </Link>
               </div>
             </div>
           ))}
