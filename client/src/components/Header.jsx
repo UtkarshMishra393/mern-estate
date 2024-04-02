9121b924ae71c428ab4adfd3e5ea243121a49e98
Utkarsh <utkarshmishra101394@gmail.com>
2024-04-02 11:43:30 +0530

    Complete header serach form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,38 +1,77 @@
-import {FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { FaSearch } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
 
 export default function Header() {
-  const {currentUser} = useSelector(state => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // URlSearchParams is a built in react method or JS constructor to set the search queries to the url.
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    // here we are gettiing the serach term query from the url.
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+    }
+  },[location.search]);
+
   return (
-    <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-        <h1 className='hover:shadow-md font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Mishra's</span>
-            <span className='text-slate-700'>Estate</span>
-        </h1>
+          <h1 className="hover:shadow-md font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Mishra's</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 md:w-64 sm:w-40'/>
-            <FaSearch className='text-state-600'/>
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 md:w-64 sm:w-40"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className="text-state-600" />
+          </button>
         </form>
-        <ul className='flex gap-4 '>
+        <ul className="flex gap-4 ">
           <Link to="/">
-          <li className='hidden sm:inline text-slate-700 hover:text-blue-700'>Home</li>
+            <li className="hidden sm:inline text-slate-700 hover:text-blue-700">
+              Home
+            </li>
           </Link>
           <Link to="about">
-          <li className='hidden sm:inline text-slate-700 hover:text-blue-700'>About</li>
+            <li className="hidden sm:inline text-slate-700 hover:text-blue-700">
+              About
+            </li>
           </Link>
           <Link to="/profile">
-          { currentUser ? (
-            <img className='rounded-full h-7 w-7 object-cover hover:border hover:border-blue-700' src={currentUser.avatar} alt="profile" />
-          ): (<li className='text-slate-700 hover:text-blue-700'>Sign In</li>
-          )
-          }
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover hover:border hover:border-blue-700"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className="text-slate-700 hover:text-blue-700">Sign In</li>
+            )}
           </Link>
         </ul>
-        </div>
+      </div>
     </header>
-  )
+  );
 }
