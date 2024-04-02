21996ccebe3be9ee244f1d61cba9543b8d49eb7c
Utkarsh <utkarshmishra101394@gmail.com>
2024-04-02 11:08:54 +0530

    Create Serach Api Route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -64,4 +64,63 @@ const getListing = async (req, res, next) => {
   }
 };
 
-module.exports = { createListng, deleteListng, updateListing, getListing };
+const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    // it will serach all the lisitngs with or without offer if offer is undefined or false.
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      // $options: "i" means it will don't care about uppercase and lowercase while searching.
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createListng,
+  deleteListng,
+  updateListing,
+  getListing,
+  getListings,
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,7 +3,10 @@ const {
   createListng,
   deleteListng,
   updateListing,
+  // getListing is to get a particular listing data to get userRef used in Listing Page.
   getListing,
+  // getListings is to get serach th listings used in Search Page.
+  getListings,
 } = require("../controllers/listing.controller");
 const verifyToken = require("../utils/verifyUser");
 
@@ -13,5 +16,6 @@ router.post("/create", verifyToken, createListng);
 router.delete("/delete/:id", verifyToken, deleteListng);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
 module.exports = router;
