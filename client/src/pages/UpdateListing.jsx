c79c61ca8e6df44b2d530ec96ed192f900c8ca7b
Utkarsh <utkarshmishra101394@gmail.com>
2024-04-03 18:17:53 +0530

    UI Changes

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -312,7 +312,7 @@ export default function CreateListing() {
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
                 {formData.type === 'rent' && (
-                  <span className='text-xs'>($ / month)</span>
+                  <span className='text-xs'>(₹ / month)</span>
                 )}
               </div>
             </div>
@@ -330,7 +330,7 @@ export default function CreateListing() {
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
                   {formData.type === 'rent' && (
-                    <span className='text-xs'>($ / month)</span>
+                    <span className='text-xs'>(₹ / month)</span>
                   )}
                 </div>
               </div>
